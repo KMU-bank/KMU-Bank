9a46cc7d15b3a3070a0ac65a6e64a52903799f34
lunahc <lunahc92@gmail.com>
2015-11-17 17:56:49 +0900

    initial commit

---
+++ src/bank/Account.java
@@ -0,0 +1,51 @@
+package bank;
+
+import java.util.LinkedList;
+
+public class Account {
+	private String accountNumber;
+	private String name;
+	private int balance;
+	private boolean haveCard = false;
+	LinkedList<String> stateList = new LinkedList<String>();
+	private boolean stolen = false;
+
+	public Account(String accountNumber, String name){
+		this.accountNumber = accountNumber;
+		this.name = name;
+		this.balance = 0;
+	}
+	
+	public Account(String accountNumber, String name, int balance){
+		this.accountNumber = accountNumber;
+		this.name = name;
+		this.balance = balance;
+	}
+	
+	public String getName(String name){
+		return name;
+	}
+	
+	public int getBalance(){
+		return balance;
+	}
+	
+	public void deposit(int money){
+		balance += money;
+	}
+	
+	public boolean withdraw(int money){
+		if(balance < money)
+			return false;
+		balance -= money;
+		return true;
+	}
+	
+	public boolean useCard(String accountNumber, String state, int money){
+		if(haveCard == false)
+			return false;
+		balance -= money;
+		stateList.add(state);
+		return true;
+	}
+}
--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -1,5 +1,40 @@
 package bank;
 
-public class Bank {
+import java.util.HashMap;
 
+public class Bank {
+	
+	private static Bank bank;
+	private HashMap<String, Account> account = new HashMap<String, Account>();
+	private int balance;
+	final private double interest = 0.1;
+	
+	public static Bank getinstance(){
+		if(bank == null)
+			bank = new Bank();
+		return bank;
+	}
+	
+	public boolean openAccount(String accountNumber, String name){//random!!!!!!!
+		if(account.get(accountNumber) != null)
+			return false;
+		account.put(accountNumber, new Account(accountNumber, name));
+		return true;
+	}
+	
+	public boolean closeAccount(String accountNumber){
+		if(account.get(accountNumber) == null)
+			return false;
+		account.remove(accountNumber);
+		return true;
+	}
+	
+	public void deposit(String accountNumber, int money){
+		account.get(accountNumber).deposit(money);
+	}
+	
+	public void withdraw(String accountNumber, int money){
+		account.get(accountNumber).withdraw(money);
+	}
+	
 }
