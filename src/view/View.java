c844deeac32744c907b9ca03186d78dc63201b36
GeniousM <sjy920404@naver.com>
2015-11-20 15:49:20 +0900

    view class 구현

--- src/view/View.java
+++ src/view/View.java
@@ -1,9 +1,58 @@
 package view;
 
 public class View {
-	public static final String title = "*****************************************************\n";
-	title += "*****************************************************\n";
-	public void page1(){
-		System.out.println(title);
+
+	public static void main(String[] args){
+		System.out.println("");
+		
+		//---------------타티틀 화면----------------------
+		
+		System.out.println("***************************************************");
+		System.out.println("*                                                 *");
+		System.out.println("*	■■■■■■                 ■    ■                 *");
+		System.out.println("*	■     ■                ■   ■                  *");
+		System.out.println("*	■     ■                ■  ■                   *");
+		System.out.println("*	■■■■■■   ■■■■   ■■   ■ ■ ■     ■■ ■■  ■   ■   *");
+		System.out.println("*	■     ■ ■    ■  ■ ■  ■ ■■ ■   ■  ■  ■ ■   ■   *");
+		System.out.println("*	■     ■ ■    ■  ■  ■ ■ ■   ■  ■  ■  ■ ■   ■   *");
+		System.out.println("*	■■■■■■   ■■■■ ■ ■   ■■ ■    ■ ■  ■  ■  ■■■    *");
+		System.out.println("*                                                 *");
+		System.out.println("***************************************************");
+
+		//--------------------시작화면---------------------
+		
+		System.out.println("	1. 사용자 선택");
+		System.out.println("	2. 사용자 생성");
+		System.out.println("	3. 사용자 삭제");
+		
+		//-------------사용자 선택 화면-------------------
+		
+		System.out.println("	1. 계정 생성");
+		System.out.println("	2. 계정 삭제");
+		System.out.println("	-----------");
+		System.out.println("	3. 입금");
+		System.out.println("	4. 출금");
+		System.out.println("	5. 초회");
+		System.out.println("	-----------");
+		System.out.println("	6. 대출");
+		System.out.println("	7. 대출 상환");
+		System.out.println("	--------------");
+		System.out.println("");
+		System.out.println("	시간이동");
+		
+		//---------------------사용자 생성 화면-------------------
+
+		System.out.println("	은행 : ");
+		System.out.println("	이름 : ");
+		System.out.println("");
+		System.out.println("	생성계좌 : ");
+		
+		//-----------------------사용자 삭제 화면------------------
+		
+		System.out.println("	");
+		System.out.println("	");
+		System.out.println("	");
+		System.out.println("	");
+		System.out.println("");
 	}
 }
