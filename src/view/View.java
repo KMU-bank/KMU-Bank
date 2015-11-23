15702222e42b756ee362d9c4c9905788ea1ae8dd
GeniousM <sjy920404@naver.com>
2015-11-23 16:32:31 +0900

    revise / view class

--- src/view/View.java
+++ src/view/View.java
@@ -2,29 +2,33 @@ package view;
 
 public class View {
 
-	public static void main(String[] args){
+	public void Title(){
 		System.out.println("");
 		
 		//---------------타티틀 화면----------------------
 		
-		System.out.println("***************************************************");
-		System.out.println("*                                                 *");
-		System.out.println("*	■■■■■■                 ■    ■                 *");
-		System.out.println("*	■     ■                ■   ■                  *");
-		System.out.println("*	■     ■                ■  ■                   *");
-		System.out.println("*	■■■■■■   ■■■■   ■■   ■ ■ ■     ■■ ■■  ■   ■   *");
-		System.out.println("*	■     ■ ■    ■  ■ ■  ■ ■■ ■   ■  ■  ■ ■   ■   *");
-		System.out.println("*	■     ■ ■    ■  ■  ■ ■ ■   ■  ■  ■  ■ ■   ■   *");
-		System.out.println("*	■■■■■■   ■■■■ ■ ■   ■■ ■    ■ ■  ■  ■  ■■■    *");
-		System.out.println("*                                                 *");
-		System.out.println("***************************************************");
-
+		System.out.println("**************************************************************************");
+		System.out.println("*                                                                        *");
+		System.out.println("*	■■■■■■                 ■       ■■ ■           ■ ■         ■      *");
+		System.out.println("*	■     ■                ■     ■■   ■■         ■■ ■         ■      *");
+		System.out.println("*	■     ■                ■   ■■     ■ ■       ■ ■ ■         ■      *");
+		System.out.println("*	■■■■■■   ■■■■   ■■   ■ ■ ■■       ■  ■     ■  ■ ■         ■      *");
+		System.out.println("*	■     ■ ■    ■  ■ ■  ■ ■■  ■■     ■   ■   ■   ■ ■         ■      *");
+		System.out.println("*	■     ■ ■    ■  ■  ■ ■ ■     ■■   ■    ■ ■    ■  ■■     ■■       *");
+		System.out.println("*	■■■■■■   ■■■■ ■ ■   ■■ ■       ■■ ■     ■     ■    ■■■■■         *");
+		System.out.println("*                                                                        *");
+		System.out.println("**************************************************************************");
+	}
+	
+	public void Start_Page(){
 		//--------------------시작화면---------------------
 		
 		System.out.println("	1. 사용자 선택");
 		System.out.println("	2. 사용자 생성");
 		System.out.println("	3. 사용자 삭제");
-		
+	}
+	
+	public void User_Select(){
 		//-------------사용자 선택 화면-------------------
 		
 		System.out.println("	1. 계정 생성");
@@ -39,14 +43,18 @@ public class View {
 		System.out.println("	--------------");
 		System.out.println("");
 		System.out.println("	시간이동");
-		
+	}
+	
+	public void User_Create(){
 		//---------------------사용자 생성 화면-------------------
 
 		System.out.println("	은행 : ");
 		System.out.println("	이름 : ");
 		System.out.println("");
 		System.out.println("	생성계좌 : ");
-		
+	}
+	
+	public void User_Delete(){
 		//-----------------------사용자 삭제 화면------------------
 		
 		System.out.println("	");
