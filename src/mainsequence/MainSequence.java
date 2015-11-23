11ff7b910ceb0ce7d6731bbc95f227e1cb8acd05
lch01387 <lch01387@google.com>
2015-11-23 16:02:48 +0900

    revise view

--- src/mainsequence/MainSequence.java
+++ src/mainsequence/MainSequence.java
@@ -1,18 +1,30 @@
 package mainsequence;
 import view.View;
+import java.io.InputStream;
+
 import client.Client;
 
 // Bridge Pattern
 // 추상class View 구현class Client
 // view class는
 public class MainSequence {
-	View view;
+	View view = new View();
 	Client client;
+	InputStream in = new InputStream(new );
 	
+	void sequence(){
+		view.mainview(); // 첫 화면
+		int input = in.nextInt();
+		if(in.nextInt() == 1){
+			view.clientView();
+		}
+	}
 	
 	public static void main(String args[]){
-		MainSequence mains;
-		mains.view.show();
+		MainSequence main = new MainSequence();
+		while(true){
+			main.sequence();
+		}
 		//input = inputstream.nextint();
 	}
 }
