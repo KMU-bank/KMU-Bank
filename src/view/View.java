ea005440e28f6f7593e9b6a47762ad3632e53dc3
GeniousM <sjy920404@naver.com>
2015-11-27 17:31:25 +0900

    edit and save

--- src/view/View.java
+++ src/view/View.java
@@ -12,46 +12,64 @@ public class View {
 		
 		//---------------타티틀 화면----------------------
 		
-		System.out.println("**************************************************************************");
-		System.out.println("*                                                                        *");
-		System.out.println("*	■■■■■■                 ■       ■■ ■           ■ ■         ■      *");
-		System.out.println("*	■     ■                ■     ■■   ■■         ■■ ■         ■      *");
-		System.out.println("*	■     ■                ■   ■■     ■ ■       ■ ■ ■         ■      *");
-		System.out.println("*	■■■■■■   ■■■■   ■■   ■ ■ ■■       ■  ■     ■  ■ ■         ■      *");
-		System.out.println("*	■     ■ ■    ■  ■ ■  ■ ■■  ■■     ■   ■   ■   ■ ■         ■      *");
-		System.out.println("*	■     ■ ■    ■  ■  ■ ■ ■     ■■   ■    ■ ■    ■  ■■     ■■       *");
-		System.out.println("*	■■■■■■   ■■■■ ■ ■   ■■ ■       ■■ ■     ■     ■    ■■■■■         *");
-		System.out.println("*                                                                        *");
-		System.out.println("**************************************************************************");
+		System.out.println("***************************************************************************");
+		System.out.println("*                                                                         *");
+		System.out.println("*       ■■■■■■                 ■       ■■ ■           ■ ■         ■       *");
+		System.out.println("*       ■     ■                ■     ■■   ■■         ■■ ■         ■       *");
+		System.out.println("*       ■     ■                ■   ■■     ■ ■       ■ ■ ■         ■       *");
+		System.out.println("*       ■■■■■■   ■■■■   ■■   ■ ■ ■■       ■  ■     ■  ■ ■         ■       *");
+		System.out.println("*       ■     ■ ■    ■  ■ ■  ■ ■■  ■■     ■   ■   ■   ■ ■         ■       *");
+		System.out.println("*       ■     ■ ■    ■  ■  ■ ■ ■     ■■   ■    ■ ■    ■  ■■     ■■        *");
+		System.out.println("*       ■■■■■■   ■■■■ ■ ■   ■■ ■       ■■ ■     ■     ■    ■■■■■          *");
+		System.out.println("*                                                                         *");
+		System.out.println("***************************************************************************");
 	}
 	
-	public void Start_Page(){
+	public int Start_Page(){
 		//--------------------시작화면---------------------
 		
 		System.out.println("	1. 사용자 선택");
 		System.out.println("	2. 사용자 생성");
 		System.out.println("	3. 사용자 삭제");
 		
-		select_Option();
+		return select_Option();
 	}
 	
 	
-	public void User_Select(){
+	public int User_Select(HashMap<Integer, Client> clients){
 		//-------------사용자 선택 화면-------------------
 		
-		System.out.println("	1. 계좌 생성");
-		System.out.println("	2. 계좌 삭제");
-		System.out.println("	-----------");
-		System.out.println("	3. 입금");
-		System.out.println("	4. 출금");
-		System.out.println("	5. 이체");		
-		System.out.println("	. 조회");
-		System.out.println("	-----------");
+		System.out.println("	사용자 리스트");
+		User_List(clients);
+		System.out.print("	사용자 선택 : ");
+		return select_Option();
+	}
+	
+	public int Bank_Select(){
+		System.out.println("	은행을 선택해주세요");
+		System.out.println("	1. KB");
+		System.out.println("	2. NH");
+		System.out.println("	3. SH");
+		
+		return select_Option();
+	}
+	
+	public int Banking(){
+		//--------------------------은행 업무 메뉴---------------
+		System.out.println("------------------------");
+		System.out.println("	1. 입금");
+		System.out.println("	2. 출금");
+		System.out.println("	3. 이체");		
+		System.out.println("	4. 조회");
+		System.out.println("------------------------");
 		System.out.println("	. 대출");
 		System.out.println("	. 대출 상환");
-		System.out.println("	--------------");
+		System.out.println("------------------------");
+		System.out.println("	2. 계좌 삭제");
 		System.out.println("");
 		System.out.println("	1년 후...");
+		
+		return select_Option();
 	}
 	
 	public void User_Create(){
@@ -74,23 +92,26 @@ public class View {
 			System.out.println(i + ". " + clients.get(i).getName());
 	}
 	
-	public void Acount_Creat(){
-		//----------------------계좌을 생성합니다.------------------
-		System.out.println("	이름을 입력해 주세요.");
-		System.out.println("	이름 : ");
-
-		System.out.println("	생성된 계좌는 (랜덤) 입니다.");
+	public void Acount_Create(String s){
+		//----------------------계좌을 생성합니다.-----------------
+		System.out.println("	생성된 계좌는 " + s + " 입니다.");
 	}
 	
-	public void Acount_Delete(){
+	public void Acount_Delete(int dep){
 		//--------------------계좌을 삭제합니다--------------------
-		System.out.println("	계좌번호를 입력해 주세요.");
-		System.out.println("	계좌번호 : ");
+		System.out.println("	계좌를 삭제합니다.");
+		System.out.println("	계좌 내의 잔금 " + dep + "원이 자동으로 출금되었습니다.");
 	}
 	
 	public void Deposit(){
 		//------------------계좌에 돈을 입금합니다---------------------
-		System.out.println("");
+		System.out.print("	입금할 금액 : ");
+		write_money();
+	}
+	
+	public void Withdraw(){
+		System.out.print("	출금할 금액 : ");
+		write_money();
 	}
 	
 	static Scanner sc = new Scanner(System.in);
@@ -102,5 +123,9 @@ public class View {
 	public void write_name(){
 		System.out.print("	이름 : ");
 		sc.next();
+	}
+	
+	public void write_money(){
+		sc.nextInt();
 	}
 }
