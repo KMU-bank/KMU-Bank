712e3a40f2a6a091bb3a846074444d68eb15569b
GeniousM <sjy920404@naver.com>
2015-11-27 20:47:23 +0900

    update predefined function

--- src/view/View.java
+++ src/view/View.java
@@ -63,12 +63,12 @@ public class View {
 		System.out.println("	3. 이체");		
 		System.out.println("	4. 조회");
 		System.out.println("------------------------");
-		System.out.println("	. 대출");
-		System.out.println("	. 대출 상환");
+		System.out.println("	5. 대출");
+		System.out.println("	6. 대출 상환");
 		System.out.println("------------------------");
-		System.out.println("	2. 계좌 삭제");
-		System.out.println("");
-		System.out.println("	1년 후...");
+		System.out.println("	7. 계좌 삭제");
+		System.out.println("------------------------");
+		System.out.println("	8. 1년 후...");
 		
 		return select_Option();
 	}
@@ -104,37 +104,57 @@ public class View {
 		System.out.println("	계좌 내의 잔금 " + dep + "원이 자동으로 출금되었습니다.");
 	}
 	
-	public void Deposit(){
+	public int Deposit(int bal){
 		//------------------계좌에 돈을 입금합니다---------------------
 		System.out.print("	입금할 금액 : ");
-		write_money();
+		int money = write_money();
+		System.out.println("------------------------------");
+		System.out.println("	현재 잔고 : " + (money + bal));
+		return money;
 	}
 	
-	public void Withdraw(){
+	public int Withdraw(int bal){
 		System.out.print("	출금할 금액 : ");
-		write_money();
+		int money = write_money();
+		System.out.println("------------------------------");
+		System.out.println("	현재 잔고 : " + (money + bal));
+		return money;
 	}
 	
-	public void Transfer(){
+	public void transfer(){
 		System.out.println("	계좌번호를 입력해 주세요.");
 		write_acount();
 		System.out.println("	보내실 금액을 입력해주세요.");
 		write_money();
 	}
 	
-	public void State_List(LinkedList<String> stateList){
+	public void state_List(LinkedList<String> stateList){
 		System.out.println("	거래 내역을 출력합니다.");
 		for(int i =0; i<stateList.size(); i++){
 			System.out.println(stateList.get(i));
 		}
 	}
 	
-	public void Loan(){
-		
+	public int loan(int debt){
+		System.out.println("	대출하실 금액을 입력해 주시기 바랍니다.");
+		System.out.print("	대출 금액 : ");
+		int money = write_money();
+		System.out.println("------------------------------");
+		System.out.println("	현재 대출 금액 : " + (money + debt));
+		return money;
 	}
 	
-	public void Repay(){
-		
+	public int repay(int debt){
+		System.out.println("	상환하실 금액을 입력해 주시기 바랍니다.");
+		System.out.print("	상환 금액 : ");
+		int money = write_money();
+		System.out.println("------------------------------");
+		System.out.println("	현제 대출 금액 : " + (debt-money));
+		return money;
+	}
+	
+	public void timeLeap(){
+		System.out.println("	1년 후로 이동합니다. (푸슝---~=★!)");
 	}
 	
 	static Scanner sc = new Scanner(System.in);
@@ -153,7 +173,7 @@ public class View {
 		sc.next();
 	}
 	
-	public void write_money(){
-		sc.nextInt();
+	public int write_money(){
+		return sc.nextInt();
 	}
 }
