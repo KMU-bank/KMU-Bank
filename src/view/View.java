b00880bcbe21bacd7447de61f0ad27ec31e2d86b
lunahc <lunahc92@gmail.com>
2015-12-02 12:29:01 +0900

    change text location

--- src/view/View.java
+++ src/view/View.java
@@ -24,9 +24,9 @@ public class View {
 //------------------------------------------------------------------------------ 시작 화면
 	
 	public void initialPage(){
-		System.out.println("	1. 사용자 생성");
-		System.out.println("	2. 사용자 삭제");
-		System.out.println("	3. 사용자 선택");
+		System.out.println(" 1. 사용자 생성");
+		System.out.println(" 2. 사용자 삭제");
+		System.out.println(" 3. 사용자 선택");
 		System.out.print("Select Number : ");
 	}
 	
@@ -37,65 +37,63 @@ public class View {
 	}
 		
 	public void selectUser(HashMap<Integer, Client> clients){
-		System.out.println("	사용자 리스트");
+		System.out.println("사용자 리스트");
 		printUserList(clients);
-		System.out.println("0. 뒤로가기");
-		System.out.print("	사용자 선택 : ");
+		System.out.print("사용자 선택 : ");
 	}
 	
 	public void createUser(){
-		System.out.println("	사용자 이름을 입력해주세요.");
-		System.out.print("	사용자 이름 : ");
+		System.out.println("사용자 이름을 입력해주세요.");
+		System.out.print(" 사용자 이름 : ");
 	}
 	
 //------------------------------------------------------------------------------ 사용자 선택 화면 이후
 
 	public void deleteUser(HashMap<Integer, Client> clients){		
-		System.out.println("	사용자 리스트");
+		System.out.println("사용자 리스트");
 		printUserList(clients);
-		System.out.print("	삭제할 사용자 : ");
-		System.out.print("Select Number : ");
+		System.out.print(" 삭제할 사용자 번호: ");
 	}
 	
 	public void selectBank(){
-		System.out.println("	은행을 선택해주세요");
-		System.out.println("	1. KB");
-		System.out.println("	2. NH");
-		System.out.println("	3. SH");
-		System.out.print("Select Number : ");
+		System.out.println("은행을 선택해주세요");
+		System.out.println("1. KB");
+		System.out.println("2. NH");
+		System.out.println("3. SH");
+		System.out.print(" Select Number : ");
 	}
 	
 	public void banking(){
-		System.out.println("	1. 입금");
-		System.out.println("	2. 출금");
-		System.out.println("	3. 이체");		
-		System.out.println("	4. 조회");
+		System.out.println("1. 입금");
+		System.out.println("2. 출금");
+		System.out.println("3. 이체");		
+		System.out.println("4. 조회");
 		System.out.println("------------------------");
-		System.out.println("	5. 대출");
-		System.out.println("	6. 대출 상환");
+		System.out.println("5. 대출");
+		System.out.println("6. 대출 상환");
 		System.out.println("------------------------");
-		System.out.println("	7. 계좌 삭제");
+		System.out.println("7. 계좌 삭제");
 		System.out.println("------------------------");
-		System.out.println("	8. 1년 후...");
-		System.out.print("Select Number : ");
+		System.out.println("8. 1년 후...");
+		System.out.print(" Select Number : ");
 	}
 	
 //------------------------------------------------------------------------------ 메뉴 함수
 	public void currentBalance(int balance){
-		System.out.println("	현재 잔고 : " + balance);
+		System.out.println(" 현재 잔고 : " + balance);
 	}
 	
 	public void currentDebt(int debt){
 		System.out.println("------------------------------");
-		System.out.println("	현재 대출 금액 : " + (debt));
+		System.out.println("현재 대출 금액 : " + (debt));
 	}
 	
 	public void deposit(){
-		System.out.print("	입금할 금액 : ");
+		System.out.print(" 입금할 금액 : ");
 	}
 	
 	public void withdraw(){
-		System.out.print("	출금할 금액 : ");
+		System.out.print(" 출금할 금액 : ");
 	}
 	
 
@@ -116,12 +114,12 @@ public class View {
 	
 	public void loan(){
 		System.out.println("대출하실 금액을 입력해 주시기 바랍니다.");
-		System.out.print("대출 금액 : ");
+		System.out.print(" 대출 금액 : ");
 	}
 	
 	public void repay(){
 		System.out.println("상환하실 금액을 입력해 주시기 바랍니다.");
-		System.out.print("상환 금액 : ");
+		System.out.print(" 상환 금액 : ");
 	}
 	
 	public void timeLeap(){
@@ -131,15 +129,15 @@ public class View {
 //------------------------------------------------------------------------------ 계정 관련 함수
 	
 	public void noAccount(){
-		System.out.print("	계좌가 존재하지 않습니다. 계좌를 생성하시겠습니까? ( y / n ) : ");
+		System.out.print("	계좌가 존재하지 않습니다.\n 계좌를 생성하시겠습니까? ( y / n ) : ");
 	}
 	
 	public void createAccount(){
-		System.out.println("	계좌가 생성되었습니다.");
+		System.out.println("계좌가 생성되었습니다.");
 	}
 	
 	public void deleteAccount(int bal){
-		System.out.println("	계좌를 삭제합니다.");
-		System.out.println("	계좌 내의 잔금 " + bal + "원이 자동으로 출금되었습니다.");
+		System.out.println("계좌를 삭제합니다.");
+		System.out.println("계좌 내의 잔금 " + bal + "원이 자동으로 출금되었습니다.");
 	}
 }
