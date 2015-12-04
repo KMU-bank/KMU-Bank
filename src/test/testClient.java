40a34a8c7d8fa313a7b4ef72849e40ce97dccaa3
lunahc <lunahc92@gmail.com>
2015-12-04 18:28:28 +0900

    change testfile

--- src/test/testClient.java
+++ src/test/testClient.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import bank.Bank;
 import bank.KBBank;
 import client.Client;
 
@@ -12,30 +13,30 @@ public class testClient {
 	@Test
 	public void testClient1(){
 		Client cl = new Client("hanul");
-		cl.selectBank(new KBBank());
-		cl.openAccount();
+		Bank bank = new KBBank();
+		cl.openAccount(bank);
 		
 		assertEquals(2000000, cl.getAsset());
 		
-		cl.deposit(50000);
-		assertEquals(50000, cl.getBalance());
+		cl.deposit(bank, 50000);
+		assertEquals(50000, cl.getBalance(bank));
 		assertEquals(1950000, cl.getAsset());
 		
-		cl.withdraw(10000);
-		assertEquals(40000, cl.getBalance());
+		cl.withdraw(bank, 10000);
+		assertEquals(40000, cl.getBalance(bank));
 		assertEquals(1960000, cl.getAsset());
 		
-		cl.loan(20000);
-		assertEquals(20000, cl.getDebt());
+		cl.loan(bank, 20000);
+		assertEquals(20000, cl.getDebt(bank));
 		assertEquals(1980000, cl.getAsset());
 		
-		cl.repay(10000);
-		assertEquals(10000, cl.getDebt());
+		cl.repay(bank, 10000);
+		assertEquals(10000, cl.getDebt(bank));
 		assertEquals(1970000, cl.getAsset());
 		
-		cl.repayOnAccount(10000);
-		assertEquals(0, cl.getDebt());
-		assertEquals(30000, cl.getBalance());
+		cl.repayOnAccount(bank, 10000);
+		assertEquals(0, cl.getDebt(bank));
+		assertEquals(30000, cl.getBalance(bank));
 		
 		assertEquals("hanul", cl.getName());
 	}
