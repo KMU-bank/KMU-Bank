101790e95084aebadadc5c5de189228d742541da
lunahc <lunahc92@gmail.com>
2015-12-07 01:23:52 +0900

    change sequence & bug fix

--- src/mainsequence/MainSequence.java
+++ src/mainsequence/MainSequence.java
@@ -2,149 +2,146 @@ package mainsequence;
 
 import java.util.Scanner;
 
-import account.Account;
-import account.Accounts;
-import bank.Bank;
-import bank.KBBank;
-import bank.NHBank;
-import bank.WooriBank;
-import client.Client;
-import client.Clients;
+import account.*;
+import bank.*;
+import client.*;
 import data.FileIO;
 import view.View;
 
 public class MainSequence {
-	View view = new View();
-	Clients clients = Clients.getInstance();
-	Scanner sc = new Scanner(System.in);
+	private final Scanner sc = new Scanner(System.in);
 
 	Client selectedClient;
 	Bank selectedBank;
 	Account selectedAccount;
-	
-	static{
-		FileIO.restoreClientsFromFile(Clients.getInstance().getClientList());
-		FileIO.restoreAccountsFromFile(Accounts.getInstance().getAccountList());
-	}
 
 	public void firstSequence() {
 		while (true) {
-			view.screenClear();
-			view.title();
-			view.initialPage();
+			FileIO.backUpClientsOnFile(Clients.getInstance().getClientList());
+			FileIO.backUpAccountsOnFile(Accounts.getInstance().getAccountList());
+			View.screenClear();
+			View.title();
+			View.initialPage();
 
 			try {
 				int select = sc.nextInt();
 
 				switch (select) {
-				case 0:
-					FileIO.backUpClientsOnFile(Clients.getInstance().getClientList());
-					FileIO.backUpAccountsOnFile(Accounts.getInstance().getAccountList());
-					break;
 				case 1:
-					view.screenClear();
-					view.title();
-					view.createUser();
-					clients.createClient(sc.next());
+					View.screenClear();
+					View.title();
+					View.createUser();
+					Clients.getInstance().createClient(sc.next());
 					sc.nextLine();
 					break;
 				case 2:
-					view.screenClear();
-					view.title();
-					view.deleteUser(clients.getClientList());
-					clients.deleteClient(sc.nextInt());
+					View.screenClear();
+					View.title();
+					View.deleteUser(Clients.getInstance().getClientList());
+					Clients.getInstance().deleteClient(sc.nextInt());
 					break;
 				case 3:
 					clientSelectSequance();
 				}
 			} catch (Exception e) {
 				sc.nextLine();
-				view.intError();
+				View.intError();
 			}
 		} // while
 	}
 
 	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택화면
 	public void clientSelectSequance() {
-		view.screenClear();
-		view.title();
-		view.selectUser(clients.getClientList());
+		while (true) {
+			View.screenClear();
+			View.title();
+			View.selectUser(Clients.getInstance().getClientList());
 
-		try {
-			int select = sc.nextInt();
+			try {
+				int select = sc.nextInt();
 
-			if (select == 0) // 0은 뒤로가기
-				firstSequence();
-			else {
-				selectedClient = clients.getClientList().get(select);
-				bankSelectSeq();
+				if (select == 0) // 0은 뒤로가기
+					break;
+				else {
+					selectedClient = Clients.getInstance().getClientList().get(select);
+					bankSelectSeq();
+				}
+				break;
+			} catch (Exception e) {
+				sc.nextLine();
+				View.intError();
 			}
-		} catch (Exception e) {
-			sc.nextLine();
-			view.intError();
-			clientSelectSequance();
-		}
+		} // while
 	}
 
 	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택 ->
 	// 은행선택 화면
 	public void bankSelectSeq() {
-		view.screenClear();
-		view.title(selectedClient);
-		view.selectBank();
+		while (true) {
+			View.screenClear();
+			View.title(selectedClient);
+			View.selectBank();
 
-		try {
-			int select = sc.nextInt();
+			try {
+				int select = sc.nextInt();
 
-			if (select == 0)
-				clientSelectSequance();
+				if (select == 0)
+					break;
+				
+				selectedBank = findBank(select);
+				if (Clients.getInstance().haveAccount(selectedBank, selectedClient))
+					bankingSeq();
+				else
+					accountErrorSeq();
 
-			selectedBank = findBank(select);
-			if (clients.haveAccount(selectedBank, selectedClient))
-				bankingSeq();
-			else
-				accountErrorSeq();
-
-		} catch (Exception e) {
-			sc.nextLine();
-			view.intError();
-			bankSelectSeq();
-		}
+			} catch (Exception e) {
+				sc.nextLine();
+				View.intError();
+			}
+		} // while
 	}
 
 	// ----------------------------------------------------- 계좌 생성시 y/n 선택
 	public void accountErrorSeq() {
-		view.screenClear();
-		view.title(selectedClient);
-		view.noAccount();
-		String select = sc.next();
 
-		if (select.equals("n")) {
-			bankSelectSeq();
-		} else if (select.equals("y")) {
-			view.createAccount(selectedClient.openAccount(selectedBank));
-			bankingSeq();
-		} else {
-			sc.nextLine();
-			System.out.println("n 또는 y 를 입력하세요.");
-			view.pressEnter();
-			accountErrorSeq();
-		}
+		while (true) {
+			View.screenClear();
+			View.title(selectedClient);
+			View.noAccount();
+
+			String select = sc.next();
+
+			if (!select.equals("n") && !select.equals("y")) {
+				sc.nextLine();
+				System.out.println("n 또는 y 를 입력하세요.");
+				View.pressEnter();
+			} else if (select.equals("n")) {
+				break;
+			} else if (select.equals("y")) {
+				View.createAccount(selectedClient.openAccount(selectedBank));
+				bankingSeq();
+				break;
+			}
+		} // while
 	}
 
 	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택 ->
 	// 은행선택 -> 뱅킹 매뉴
 	public void bankingSeq() {
-		while (true) {
-			view.screenClear();
-			view.title(selectedClient, selectedBank);
-			view.banking();
+		loop: while (true) {
+			FileIO.backUpClientsOnFile(Clients.getInstance().getClientList());
+			FileIO.backUpAccountsOnFile(Accounts.getInstance().getAccountList());
+
+			View.screenClear();
+			View.title(selectedClient, selectedBank);
+			View.banking();
+
 			try {
 				int select = Integer.parseInt(sc.next());
+				
 				switch (select) {
 				case 0:
-					bankSelectSeq();
-					break;
+					break loop;
 				case 1:
 					depositSeq();
 					break;
@@ -164,137 +161,159 @@ public class MainSequence {
 					repaySeq();
 					break;
 				case 7:
-					deleteBank();
+					deleteBankSeq();
+					break loop;
 				case 8:
 					timeLeapSeq();
 				}
 			} catch (Exception e) {
 				sc.nextLine();
-				view.intError();
+				View.intError();
 				bankingSeq();
 			}
-		}
+		} // while
+
+		FileIO.backUpClientsOnFile(Clients.getInstance().getClientList());
+		FileIO.backUpAccountsOnFile(Accounts.getInstance().getAccountList());
+
 	}
 
 	public void depositSeq() {
-		view.screenClear();
-		view.title(selectedClient, selectedBank);
-		view.deposit();
-		try {
-			int money = sc.nextInt();
-			if (!selectedClient.deposit(selectedBank, money))
-				view.notEnoughAssetError();
-			view.currentBalance(selectedClient.getBalance(selectedBank));
-			view.pressEnter();
-		} catch (Exception e) {
-			sc.nextLine();
-			view.intError();
-			depositSeq();
-		}
+		while (true) {
+			View.screenClear();
+			View.title(selectedClient, selectedBank);
+			View.deposit();
+
+			try {
+				int money = sc.nextInt();
+				if (!selectedClient.deposit(selectedBank, money))
+					View.notEnoughAssetError();
+
+				View.currentBalance(selectedClient.getBalance(selectedBank));
+				View.pressEnter();
+				break;
+			} catch (Exception e) {
+				sc.nextLine();
+				View.intError();
+			}
+		} // while
 	}
 
 	public void withdrawSeq() {
-		view.screenClear();
-		view.title(selectedClient, selectedBank);
-		view.withdraw();
-		try {
-			int money = sc.nextInt();
-			if (!selectedClient.withdraw(selectedBank, money))
-				view.notEnoughBalanceError();
+		while (true) {
+			View.screenClear();
+			View.title(selectedClient, selectedBank);
+			View.withdraw();
 
-			view.currentBalance(selectedClient.getBalance(selectedBank));
-			view.pressEnter();
-		} catch (Exception e) {
-			sc.nextLine();
-			view.intError();
-			withdrawSeq();
-		}
+			try {
+				int money = sc.nextInt();
+				if (!selectedClient.withdraw(selectedBank, money))
+					View.notEnoughBalanceError();
+
+				View.currentBalance(selectedClient.getBalance(selectedBank));
+				View.pressEnter();
+				break;
+			} catch (Exception e) {
+				sc.nextLine();
+				View.intError();
+			}
+		} // while
 	}
 
 	public void transferSeq() {
 		while (true) {
-			view.screenClear();
-			view.title(selectedClient, selectedBank);
-			view.transferAccountNumber();
+			View.screenClear();
+			View.title(selectedClient, selectedBank);
+			View.transferAccountNumber();
 
 			String accountNumber = sc.next();
+
 			if (accountNumber.equals(selectedClient.getAccountNumber(selectedBank))) {
 				System.out.println("자신의 계좌로 입금할 수 없습니다.");
-				view.pressEnter();
+				View.pressEnter();
 				break;
-			}
+			} else if (accountNumber.equals("-1")) {
+				View.printAccountNumber();
+				accountNumber = sc.next();
+			} // 계좌번호를 모를때 모든 client의 계좌번호를 출력한다
 
 			try {
-				view.transferMoney();
-				int money = sc.nextInt();
-				if (!selectedClient.transfer(selectedBank, accountNumber, money))
-					view.notEnoughBalanceError();
+				View.transferMoney();
 
-				view.currentBalance(selectedClient.getBalance(selectedBank));
-				view.pressEnter();
+				int money = sc.nextInt();
+
+				if (!selectedClient.transfer(selectedBank, accountNumber, money))
+					View.notEnoughBalanceError();
+
+				View.currentBalance(selectedClient.getBalance(selectedBank));
+				View.pressEnter();
 				break;
 			} catch (Exception e) {
 				sc.nextLine();
-				view.intError();
+				View.intError();
 			}
-		}
+		} // while
 	}
 
 	public void printStateSeq() {
-		System.out.println("계좌번호 : " + selectedClient.getAccountNumber(selectedBank));
-		view.printStateList(selectedClient.getStateList(selectedBank));
-		view.pressEnter();
+		View.printStateList(selectedClient.getStateList(selectedBank));
+		View.pressEnter();
 	}
 
 	public void loanSeq() {
-		view.screenClear();
-		view.title(selectedClient, selectedBank);
-		view.loan();
+		while (true) {
+			View.screenClear();
+			View.title(selectedClient, selectedBank);
+			View.loan();
 
-		try {
-			int money = sc.nextInt();
-			selectedClient.loan(selectedBank, money);
-			view.currentDebt(selectedClient.getDebt(selectedBank));
-			view.pressEnter();
-		} catch (Exception e) {
-			sc.nextLine();
-			view.intError();
-			loanSeq();
-		}
+			try {
+				int money = sc.nextInt();
+
+				selectedClient.loan(selectedBank, money);
+
+				View.currentDebt(selectedClient.getDebt(selectedBank));
+				View.pressEnter();
+				break;
+			} catch (Exception e) {
+				sc.nextLine();
+				View.intError();
+			}
+		} // while
 	}
 
 	public void repaySeq() {
-		view.screenClear();
-		view.title(selectedClient, selectedBank);
-		view.repay();
+		while (true) {
+			View.screenClear();
+			View.title(selectedClient, selectedBank);
+			View.repay();
 
-		try {
-			int money = sc.nextInt();
-			selectedClient.repay(selectedBank, money);
-			view.currentDebt(selectedClient.getDebt(selectedBank));
-			view.pressEnter();
-		} catch (Exception e) {
-			sc.nextLine();
-			view.intError();
-			repaySeq();
-		}
+			try {
+				int money = sc.nextInt();
+
+				selectedClient.repay(selectedBank, money);
+
+				View.currentDebt(selectedClient.getDebt(selectedBank));
+				View.pressEnter();
+				break;
+			} catch (Exception e) {
+				sc.nextLine();
+				View.intError();
+			}
+		} // while
 	}
 
 	public void timeLeapSeq() {
-		view.screenClear();
-		view.title(selectedClient, selectedBank);
-		view.timeLeap();
+		View.screenClear();
+		View.title(selectedClient, selectedBank);
+		View.timeLeap();
 
 		selectedClient.timeLeap(selectedBank);
-		view.pressEnter();
+		View.pressEnter();
 	}
 
-	public void deleteBank() {
-		view.deleteAccount();
-
-		selectedClient.closeAccount(selectedBank);
-		view.pressEnter();
-		bankSelectSeq();
+	public void deleteBankSeq() {
+		View.deleteAccount(selectedClient.closeAccount(selectedBank));
+		View.pressEnter();
 	}
 
 	public Bank findBank(int select) {
@@ -307,6 +326,9 @@ public class MainSequence {
 	}
 
 	public static void main(String args[]) {
+		FileIO.restoreClientsFromFile(Clients.getInstance().getClientList());
+		FileIO.restoreAccountsFromFile(Accounts.getInstance().getAccountList());
+
 		MainSequence seq = new MainSequence();
 		seq.firstSequence();
 	}
