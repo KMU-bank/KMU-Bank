9073e2541da16622369e19b65aeaf7ece645e283
GeniousM <sjy920404@naver.com>
2015-12-11 17:10:40 +0900

    add comment of View and MainSequence

--- src/mainsequence/MainSequence.java
+++ src/mainsequence/MainSequence.java
@@ -27,20 +27,20 @@ public class MainSequence {
 				int select = sc.nextInt();
 
 				switch (select) {
-				case 1:
+				case 1:																// 사용자 생성
 					View.screenClear();
 					View.title();
 					View.createUser();
 					Clients.getInstance().createClient(sc.next());
 					sc.nextLine();
 					break;
-				case 2:
+				case 2:																// 사용자 삭제
 					View.screenClear();
 					View.title();
 					View.deleteUser(Clients.getInstance().getClientList());
 					Clients.getInstance().deleteClient(sc.nextInt());
 					break;
-				case 3:
+				case 3:																// 사용자 선택
 					clientSelectSequance();
 				}
 			} catch (Exception e) {
@@ -60,9 +60,9 @@ public class MainSequence {
 			try {
 				int select = sc.nextInt();
 
-				if (select == 0) // 0은 뒤로가기
+				if (select == 0)	// 0은 뒤로가기
 					break;
-				else {
+				else {				// 사용자 번호 선택
 					selectedClient = Clients.getInstance().getClientList().get(select);
 					bankSelectSeq();
 				}
@@ -89,9 +89,9 @@ public class MainSequence {
 					break;
 
 				selectedBank = findBank(select);
-				if (Clients.getInstance().haveAccount(selectedBank, selectedClient))
+				if (Clients.getInstance().haveAccount(selectedBank, selectedClient))	// 은행 선택
 					bankingSeq();
-				else
+				else																	// 1,2,3,0 외의 잘못된 입력시 ERROR 출력
 					accountErrorSeq();
 
 			} catch (Exception e) {
@@ -111,13 +111,13 @@ public class MainSequence {
 
 			String select = sc.next();
 
-			if (!select.equals("n") && !select.equals("y")) {
+			if (!select.equals("n") && !select.equals("y")) {					// y/n 외 다른 입력시 경고 메시지 출력
 				sc.nextLine();
 				System.out.println("n 또는 y 를 입력하세요.");
 				View.pressEnter();
-			} else if (select.equals("n")) {
+			} else if (select.equals("n")) {									// n 입력, 뒤로 돌아감
 				break;
-			} else if (select.equals("y")) {
+			} else if (select.equals("y")) {									// y 입력, 계좌 생성 후 뱅킹 메뉴로 이동
 				View.createAccount(selectedClient.openAccount(selectedBank));
 				bankingSeq();
 				break;
@@ -142,28 +142,28 @@ public class MainSequence {
 				switch (select) {
 				case 0:
 					break loop;
-				case 1:
+				case 1:						// 입금
 					depositSeq();
 					break;
-				case 2:
+				case 2:						// 출금
 					withdrawSeq();
 					break;
-				case 3:
+				case 3:						// 이체
 					transferSeq();
 					break;
-				case 4:
+				case 4:						// 조회
 					printStateSeq();
 					break;
-				case 5:
+				case 5:						// 대출
 					loanSeq();
 					break;
-				case 6:
+				case 6:						// 대출 상환
 					repaySeq();
 					break;
-				case 7:
+				case 7:						// 계좌 삭제
 					deleteBankSeq();
 					break loop;
-				case 8:
+				case 8:						// 1년 시간이동
 					timeLeapSeq();
 				}
 			} catch (Exception e) {
@@ -178,6 +178,8 @@ public class MainSequence {
 
 	}
 
+	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택 -> 은행선택 ->
+	// 뱅킹 매뉴 -> 입금
 	public void depositSeq() {
 		while (true) {
 			View.screenClear();
@@ -186,9 +188,9 @@ public class MainSequence {
 
 			try {
 				int money = sc.nextInt();
-				if (money == 0)
+				if (money == 0)		// 0은 언제나 취소
 					break;
-				if (!selectedClient.deposit(selectedBank, money))
+				if (!selectedClient.deposit(selectedBank, money))	// 현금이 입금액보다 부족할 시 ERROR
 					View.notEnoughAssetError();
 
 				View.currentBalance(selectedClient.getBalance(selectedBank));
@@ -201,6 +203,8 @@ public class MainSequence {
 		} // while
 	}
 
+	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택 -> 은행선택 ->
+	// 뱅킹 매뉴 -> 출금
 	public void withdrawSeq() {
 		while (true) {
 			View.screenClear();
@@ -209,9 +213,9 @@ public class MainSequence {
 
 			try {
 				int money = sc.nextInt();
-				if (money == 0)
+				if (money == 0)		// 0은 언제나 취소
 					break;
-				if (!selectedClient.withdraw(selectedBank, money))
+				if (!selectedClient.withdraw(selectedBank, money))		// 출금할 계좌 잔금이 출금액보다 적을시 ERROR
 					View.notEnoughBalanceError();
 
 				View.currentBalance(selectedClient.getBalance(selectedBank));
@@ -224,6 +228,8 @@ public class MainSequence {
 		} // while
 	}
 
+	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택 -> 은행선택 ->
+	// 뱅킹 매뉴 -> 이체
 	public void transferSeq() {
 		while (true) {
 			View.screenClear();
@@ -232,26 +238,26 @@ public class MainSequence {
 
 			String accountNumber = sc.next();
 
-			if (accountNumber.equals(selectedClient.getAccountNumber(selectedBank))) {
+			if (accountNumber.equals(selectedClient.getAccountNumber(selectedBank))) {		// 이체시 자기 계좌에서 자기 계좌로의 이체는 불가능 합니다.
 				System.out.println("자신의 계좌로 입금할 수 없습니다.");
 				View.pressEnter();
 				break;
-			} else if (accountNumber.equals("-1")) {
+			} else if (accountNumber.equals("-1")) {		// 본래는 계좌번호를 따로 알아야 하지만, 편의상 현제 개설된 사용자-계좌 를 확인 할 수 있는 커멘드입니다.
 				View.printAccountNumber();
 				accountNumber = sc.next();
 			} // 계좌번호를 모를때 모든 client의 계좌번호를 출력한다
 
-			if (accountNumber.equals("0"))
+			if (accountNumber.equals("0"))		// 0은 언제나 취소
 				break;
 
 			try {
 				View.transferMoney();
 
 				int money = sc.nextInt();
-				if (money == 0)
+				if (money == 0)			// 0은 언제나 취소
 					break;
 
-				if (!selectedClient.transfer(selectedBank, accountNumber, money))
+				if (!selectedClient.transfer(selectedBank, accountNumber, money))	// 이체시 자신의 계좌 잔금이 송금할 금액보다 적을시 ERROR
 					View.notEnoughBalanceError();
 
 				View.currentBalance(selectedClient.getBalance(selectedBank));
@@ -264,11 +270,15 @@ public class MainSequence {
 		} // while
 	}
 
+	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택 -> 은행선택 ->
+	// 뱅킹 매뉴 -> 조회
 	public void printStateSeq() {
 		View.printStateList(selectedClient.getStateList(selectedBank));
 		View.pressEnter();
 	}
 
+	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택 -> 은행선택 ->
+	// 뱅킹 매뉴 -> 대출
 	public void loanSeq() {
 		while (true) {
 			View.screenClear();
@@ -277,10 +287,10 @@ public class MainSequence {
 
 			try {
 				int money = sc.nextInt();
-				if (money == 0)
+				if (money == 0)			// 0은 언제나 취소
 					break;
 
-				if (selectedClient.loan(selectedBank, money)) {
+				if (selectedClient.loan(selectedBank, money)) {			// 대출시 대출 금 만큼 현금과 부체가 증가합니다.
 					View.currentDebt(selectedClient.getDebt(selectedBank));
 					View.pressEnter();
 					break;
@@ -295,6 +305,8 @@ public class MainSequence {
 		} // while
 	}
 
+	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택 -> 은행선택 ->
+	// 뱅킹 매뉴 -> 대출 상환
 	public void repaySeq() {
 		while (true) {
 			View.screenClear();
@@ -303,19 +315,19 @@ public class MainSequence {
 
 			try {
 				int money = sc.nextInt();
-				if (money == 0)
+				if (money == 0)			// 0은 언제나 취소
 					break;
 
 				View.selectAssetOrAccount();
 				int select = sc.nextInt();
-				if (select == 0)
+				if (select == 0)		// 0은 언제나 취소
 					break;
-				else if (select == 1 && !selectedClient.repay(selectedBank, money))
+				else if (select == 1 && !selectedClient.repay(selectedBank, money))				// 부체상환 방법으로 현금을 선택합니다.
 					View.notEnoughAssetError();
-				else if (select == 2 && !selectedClient.repayOnAccount(selectedBank, money))
-					View.notEnoughBalanceError();
+				else if (select == 2 && !selectedClient.repayOnAccount(selectedBank, money))	// 부체상환 방법으로 계좌를 선택합니다.
+					View.notEnoughBalanceError();												// 상환시 해당 방법의 잔액이 상환금액보다 적을시 ERROR
 				else{
-					View.unvalidInput();
+					View.unvalidInput();		// 상환한 금액을 확인 시켜주고 Enter를 칠때까지 대기합니다.
 					View.pressEnter();
 					break;
 				}
@@ -330,29 +342,36 @@ public class MainSequence {
 		} // while
 	}
 
+	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택 -> 은행선택 ->
+	// 뱅킹 매뉴 -> 1년 이동
 	public void timeLeapSeq() {
 		View.screenClear();
-		View.title(selectedClient, selectedBank);
+		View.title(selectedClient, selectedBank);		// 현실시간과 연동 돼진 않았지만, 시간의 흐름에 따른 이자도 구현해냈습니다.
 		View.timeLeap();
 
 		selectedClient.timeLeap(selectedBank);
 		View.pressEnter();
 	}
 
+	// ----------------------------------------------------- 시작화면 -> 3.사용자 선택 -> 은행선택 ->
+	// 뱅킹 매뉴 -> 계좌 삭제
 	public void deleteBankSeq() {
-		View.deleteAccount(selectedClient.closeAccount(selectedBank));
+		View.deleteAccount(selectedClient.closeAccount(selectedBank));		// 계좌를 삭제합니다. 계좌 잔액과 미상환 대출금이 있다면 계산되어 현금으로 들어갑니다.
 		View.pressEnter();
 	}
 
+	// ----------------------------------------------------- 시작화면 ->
+	// 사용자 선택 -> 은행선택
 	public Bank findBank(int select) {
-		if (select == 1)
+		if (select == 1)				// 은행 KB 를 선택합니다
 			return new KBBank();
-		else if (select == 2)
+		else if (select == 2)			// 은행 NH 를 선택합니다
 			return new NHBank();
-		else
+		else							// 은행 Woori 를 선택합니다
 			return new WooriBank();
 	}
 
+	// ----------------------------------------------------- 시작화면 - 메인 시퀀스 시작
 	public static void main(String args[]) {
 		FileIO.restoreClientsFromFile(Clients.getInstance().getClientList());
 		FileIO.restoreAccountsFromFile(Accounts.getInstance().getAccountList());
--- src/view/View.java
+++ src/view/View.java
@@ -13,6 +13,7 @@ import client.Clients;
 
 public class View {
 
+	// 간판 로고 출력 [ BanKMU ]
 	public static final void OriginalTitle() {
 		System.out.println("***************************************************************************");
 		System.out.println("*                                                                         *");
@@ -32,6 +33,7 @@ public class View {
 		OriginalTitle();
 	}
 
+	// 사용자 선택 후 은행 선택 화면에서 현재 사용자의 이름과 자산(현금)을 출력해줍니다.
 	public static final void title(Client selectedClient) {
 		OriginalTitle();
 		System.out
@@ -39,20 +41,22 @@ public class View {
 		System.out.println("---------------------------------------------------------------------------");
 	}
 
+	// 은행 선택 후 은행업무 화면에서의 현재 사용자의 자금 현황을 출력합니다.
 	public static final void title(Client selectedClient, Bank selectedBank) {
 		String accountNumber = selectedClient.getAccountNumber(selectedBank);
 		OriginalTitle();
-		System.out.print("현재 선택된 사용자 : " + selectedClient.getName() + " 현재 선택된 사용자의 자산 : " + selectedClient.getAsset()
-				+ " 현재 선택된 계좌의 계좌번호 : " + accountNumber + "\n현재 선택된 은행계좌의 잔고 : "
+		System.out.print("현재 선택된 사용자 : " + selectedClient.getName() + "		현재 선택된 사용자의 자산 : " + selectedClient.getAsset()
+				+ "\n현재 선택된 계좌의 계좌번호 : " + accountNumber + "	현재 선택된 은행계좌의 잔고 : "
 				+ selectedBank.getBalance(accountNumber));
 		if (selectedBank.getDebt(accountNumber) != 0)
-			System.out.print(" 현재 선택된 은행계좌의 대출금 : " + selectedBank.getDebt(accountNumber));
+			System.out.print("	현재 선택된 은행계좌의 대출금 : " + selectedBank.getDebt(accountNumber));
 		System.out.println("\n---------------------------------------------------------------------------");
 	}
 
 	// ------------------------------------------------------------------------------
 	// 시작 화면
 
+	// 프로그램 시작, 첫 화면입니다.
 	public static final void initialPage() {
 		System.out.println(" 1. 사용자 생성");
 		System.out.println(" 2. 사용자 삭제");
@@ -60,18 +64,21 @@ public class View {
 		System.out.print("Select Number : ");
 	}
 
+	// 사용자리스트를 출력합니다. 사용자 삭제, 사용자 선택에 이용됩니다.
 	public static final void printUserList(HashMap<Integer, Client> clients) {
 		for (int i = 1; i < 100; i++)
 			if (clients.get(i) != null)
 				System.out.println(i + ". " + clients.get(i).getName());
 	}
 
+	// 사용자 선택 화면입니다. 사용자 리스트와 선택 콘솔에서 번호로 사용자를 선택합니다.
 	public static final void selectUser(HashMap<Integer, Client> clients) {
 		System.out.println("사용자 리스트");
 		printUserList(clients);
 		System.out.print("사용자 선택 : ");
 	}
 
+	// 사용자 생성 화면입니다. 사용자 이름을 입력하면 계정생성과 동시에 기본 자금 3,000,000원 이 생성됩니다.
 	public static final void createUser() {
 		System.out.println("사용자 이름을 입력해주세요.");
 		System.out.print(" 사용자 이름 : ");
@@ -80,12 +87,14 @@ public class View {
 	// ------------------------------------------------------------------------------
 	// 사용자 선택 화면 이후
 
+	// 사용자 삭제 화면입니다. 사용자 리스트를 출력하고 삭제할 사용자의 번호를 입력하면 해당사용자의 모든 정보가 삭제됩니다.
 	public static final void deleteUser(HashMap<Integer, Client> clients) {
 		System.out.println("사용자 리스트");
 		printUserList(clients);
 		System.out.print(" 삭제할 사용자 번호: ");
 	}
 
+	// 은행 선택 화면입니다. 사용자는 총 3개의 은행중에 사용할 은행을 자유로이 선택해 사용 할 수 있습니다.
 	public static final void selectBank() {
 		System.out.println("은행을 선택해주세요");
 		System.out.println("1. KB");
@@ -94,6 +103,7 @@ public class View {
 		System.out.print(" Select Number : ");
 	}
 
+	// 은행 업무 화면입니다. 이 화면에서 사용자는 본격적인 은행 업무를 볼 수 있습니다.
 	public static final void banking() {
 		System.out.println("1. 입금");
 		System.out.println("2. 출금");
@@ -111,29 +121,36 @@ public class View {
 
 	// ------------------------------------------------------------------------------
 	// 메뉴 함수
+	
+	// 첫번 째 대분류 메뉴인 입금, 출금, 이체, 조회 업무에서 업무를 마치고 난 다음의 잔고를 출력합니다.
 	public static final void currentBalance(int balance) {
 		System.out.println(" 현재 잔고 : " + balance);
 	}
 
+	// 두번 째 대분류 메뉴인 대출, 대출상환 업무에서 남아있는 대출금의 잔고를 출력합니다.
 	public static final void currentDebt(int debt) {
 		System.out.println("현재 대출 금액 : " + (debt));
 		System.out.println("------------------------------");
 	}
 
+	// 입금 메뉴에서 입금할 금액을 입력해줍니다.
 	public static final void deposit() {
 		System.out.print(" 입금할 금액 : ");
 	}
 
+	// 출금 메뉴에서 출금할 금액을 입력해줍니다.
 	public static final void withdraw() {
 		System.out.print(" 출금할 금액 : ");
 	}
 
+	// 계좌이체 화면에서 입금대상 계좌번호를 입력합니다. 입금할 대상의 계좌번호를 모를시 -1을 입력하면 모든 사용자의 모든 은행 계좌를 보여줍니다.
 	public static final void transferAccountNumber() {
 		System.out.println("계좌번호를 입력해 주세요.");
 		System.out.println("계좌번호를 모르시면 -1을 입력해 주세요.");
 		System.out.print("계좌번호 : ");
 	}
 
+	// 계좌이체 화면에서 -1을 입력할시 나오는 사용자-계좌 리스트를 출력하고 계좌번호 입력 화면입니다.
 	public static final void printAccountNumber() {
 		for(int key: Clients.getInstance().getClientList().keySet()){
 			Client client = Clients.getInstance().getClientList().get(key);
@@ -148,10 +165,12 @@ public class View {
 		System.out.print(" 계좌번호 : ");
 	}
 
+	// 계좌번호를 입련한 후, 송금할 금액을 입력해줍니다. 
 	public static final void transferMoney() {
 		System.out.print("보내실 금액을 입력해주세요.\n 금액 : ");
 	}
 
+	// 조회 화면입니다. 지금까지 거래한 모든 거래 내역을 출력합니다.
 	public static final void printStateList(LinkedList<String> stateList) {
 		System.out.println("거래 내역을 출력합니다.");
 		for (int i = 0; i < stateList.size(); i++) {
@@ -159,16 +178,19 @@ public class View {
 		}
 	}
 
+	// 대출 화면입니다. 대출할 금액을 입력합니다.
 	public static final void loan() {
 		System.out.println("대출하실 금액을 입력해 주시기 바랍니다.");
 		System.out.print(" 대출 금액 : ");
 	}
 
+	// 대출 상환 화면입니다. 상환할 금액을 입력합니다.
 	public static final void repay() {
 		System.out.println("상환하실 금액을 입력해 주시기 바랍니다.");
 		System.out.print(" 상환 금액 : ");
 	}
 	
+	// 가지고 있는 현금과 계좌내의 금액중에 상환의 대상이 될 방법을 선택합니다.
 	public static final void selectAssetOrAccount(){
 		System.out.println("피상환 개체를 선택해 주시기 바랍니다.");
 		System.out.println("1. My Asset");
@@ -176,6 +198,7 @@ public class View {
 		System.out.print(" Select Number : ");
 	}
 
+	// 1년 후로 이동합니다. (푸슝---~=★!)
 	public static final void timeLeap() {
 		System.out.println("	1년 후로 이동합니다. (푸슝---~=★!)");
 	}
@@ -183,10 +206,12 @@ public class View {
 	// ------------------------------------------------------------------------------
 	// 계정 관련 함수
 
+	// 계정 생성 후, 아직 해당 은행 계좌가 존재하지 않는다면 은행업무를 볼 수 없기에 계좌 생성 화면을 거칩니다. y/n 로 생성할지 말지를 선택 할 수 있습니다.
 	public static final void noAccount() {
 		System.out.print("	계좌가 존재하지 않습니다.\n 계좌를 생성하시겠습니까? ( y / n ) : ");
 	}
 
+	// 계좌 생성여부에서 y를 선택하면 난수를 거쳐 일정한 형식에 의해 계좌가 생성됩니다.
 	public static final void createAccount(String accountNumber) {
 		System.out.println("계좌가 생성되었습니다.");
 		System.out.println("계좌번호는 : " + accountNumber + "입니다.");
@@ -198,6 +223,7 @@ public class View {
 		}
 	}
 
+	// 계좌 삭제 화면입니다. 잔금 혹은 미상환 대출금이 있을지 자동으로 계산되어 현금에 적용됩니다.
 	public static final void deleteAccount(int bal) {
 		System.out.println("계좌를 삭제합니다.");
 		System.out.println("계좌 내의 잔금 " + bal + "원이 자동으로 출금되었습니다.");
@@ -205,7 +231,6 @@ public class View {
 
 	// ------------------------------------------------------------------------------
 	// 화면 밀어내기 ( cls )
-
 	public static final void screenClear() {
 		for (int i = 0; i < 80; i++)
 			System.out.println("");
@@ -214,6 +239,7 @@ public class View {
 	// ------------------------------------------------------------------------------
 	// Error print functions...
 
+	// 입금, 출금, 등 자산의 변화가 있는 작업을 수행 한 후, 변경사항을 보여주기 전에 넘어가게 하지 않기 위해 잠시 대기할 수 있는 화면입니다.
 	public static final void pressEnter() {
 		System.out.print("계속하려면 엔터를 누르세요.");
 		try {
@@ -223,19 +249,23 @@ public class View {
 		}
 	}
 
+	// 숫자가 입력돼야 할 입력칸에 문자열이 입력돼었을 시 출력되는 경고 메시지 입니다.
 	public static final void intError() {
 		System.out.println("숫자만 입력하세요.");
 		pressEnter();
 	}
 
+	// 현금 -> 계좌로 입금시 현금 이상의 금액이 입력되면 출력되는 경고 메시지 입니다.
 	public static final void notEnoughAssetError() {
 		System.out.println("소지금이 부족합니다.");
 	}
 
+	// 계좌에서 출금이 이뤄지는 작업수행중에 출금액이 계좌의 잔액보다 클시 출력되는 경고 메시지 입니다.
 	public static final void notEnoughBalanceError() {
 		System.out.println("계좌에 잔액이 부족합니다.");
 	}
 	
+	// 문자열입력이 필요한 부분에 숫자를 입력하거나, 숫자입력이 필요한 부분에 문자열을 입력하는 등 기타 의도하지 않은 입력으로인한 접근시 출력되는 경고 메시지 입니다.
 	public static final void unvalidInput(){
 		System.out.println("잘못된 입력입니다.");
 	}
